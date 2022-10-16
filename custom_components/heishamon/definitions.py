8cc31a78927e16c7e2ae3fdb7afe508081df53b1
Grégoire Seux <kamaradclimber@gmail.com>
2022-10-16 20:10:38 +0200

    ✨ Support switch
    Switches can replace some of the binary sensors
    
    Change-Id: I6570de7e653f55835ade2b58c8ebdbca8983e170

--- custom_components/heishamon/definitions.py
+++ custom_components/heishamon/definitions.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 from collections.abc import Callable
 from dataclasses import dataclass
 from typing import Optional
+import logging
+
+from homeassistant.components.switch import SwitchEntityDescription, SwitchDeviceClass
 
 from homeassistant.components.sensor import (
     SensorDeviceClass,
@@ -12,6 +15,7 @@ from homeassistant.components.sensor import (
 )
 from homeassistant.components.binary_sensor import (
     BinarySensorDeviceClass,
+    BinarySensorEntityDescription,
 )
 from homeassistant.const import (
     ELECTRIC_CURRENT_AMPERE,
@@ -20,6 +24,8 @@ from homeassistant.const import (
     POWER_KILO_WATT,
     VOLUME_CUBIC_METERS,
 )
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def read_operating_mode_state(value):
@@ -35,11 +41,46 @@ def read_operating_mode_state(value):
     return values.get(value, f"Unknown operating mode value")
 
 
+def read_power_mode_time(value):
+    return int(value) * 30
+
+
+def read_threeway_valve(value: str) -> Optional[str]:
+    if value == "0":
+        return "Room"
+    elif value == "1":
+        return "Tank"
+    else:
+        _LOGGER.info(f"Reading unhandled value for ThreeWay Valve state: '{value}'")
+        return None
+
+
 @dataclass
 class HeishaMonSensorEntityDescription(SensorEntityDescription):
     """Sensor entity description for HeishaMon."""
 
     # a method called when receiving a new value
+    state: Callable | None = None
+
+
+@dataclass
+class HeishaMonSwitchEntityDescription(SwitchEntityDescription):
+    """Switch entity description for HeishaMon."""
+
+    command_topic: str = "void/topic"
+    qos: int = 0
+    payload_on: str = "1"
+    payload_off: str = "0"
+    retain: bool = False
+    encoding: str = "utf-8"
+    # a method called when receiving a new value
+    state: Callable | None = None
+
+
+@dataclass
+class HeishaMonBinarySensorEntityDescription(BinarySensorEntityDescription):
+    """Binary sensor entity description for HeishaMon."""
+
     state: Callable | None = None
 
 
@@ -52,21 +93,76 @@ def bit_to_bool(value: str) -> Optional[bool]:
         return None
 
 
-BINARY_SENSORS: tuple[HeishaMonSensorEntityDescription, ...] = (
-    HeishaMonSensorEntityDescription(
+def read_quiet_mode(value: str) -> Optional[bool]:
+    if value == "4":
+        return True  # Scheduled
+    elif value == "0":
+        return False
+    _LOGGER.info(f"Reading unhandled quiet mode: '{value}'")
+    return None
+
+
+MQTT_SWITCHES: tuple[HeishaMonSwitchEntityDescription, ...] = (
+    HeishaMonSwitchEntityDescription(
+        key="panasonic_heat_pump/main/Holiday_Mode_State",
+        command_topic="panasonic_heat_pump/main/Holiday_Mode_State",
+        name="Aquarea Holiday Mode",
+        state=bit_to_bool,  # FIXME: support this
+    ),
+    HeishaMonSwitchEntityDescription(
         key="panasonic_heat_pump/main/Heatpump_State",
-        name="Aquarea Power State",
+        command_topic="panasonic_heat_pump/commands/SetHeatpump",
+        name="Aquarea Main Power",
         state=bit_to_bool,
         device_class=BinarySensorDeviceClass.RUNNING,
+    ),
+    HeishaMonSwitchEntityDescription(
+        key="panasonic_heat_pump/main/Force_DHW_State",
+        command_topic="panasonic_heat_pump/commands/SetForceDHW",
+        name="Aquarea Force DHW Mode",
+        state=bit_to_bool,
+    ),
+)
+
+BINARY_SENSORS: tuple[HeishaMonBinarySensorEntityDescription, ...] = (
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/Quiet_Mode_Level",
+        name="Aquarea Quiet Mode",
+        state=read_quiet_mode,
         # state_class=SensorStateClass.MEASUREMENT,
         # icon= "mdi:on"
         # entity_registry_enabled_default=True,
         # native_unit_of_measurement="L/min",
     ),
-    HeishaMonSensorEntityDescription(
-        key="panasonic_heat_pump/main/Force_DHW_State",
-        name="Aquarea Force DHW Mode",
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/Defrosting_State",
+        name="Aquarea Defrost State",
         state=bit_to_bool,
+        device_class=BinarySensorDeviceClass.HEAT,
+    ),
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/DHW_Heater_State",
+        name="Aquarea Tank Heater Enabled",
+        state=bit_to_bool,
+        device_class=BinarySensorDeviceClass.HEAT,
+    ),
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/Room_Heater_State",
+        name="Aquarea Room Heater Enabled",
+        state=bit_to_bool,
+        device_class=BinarySensorDeviceClass.HEAT,
+    ),
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/Internal_Heater_State",
+        name="Aquarea Internal Heater State",
+        state=bit_to_bool,
+        device_class=BinarySensorDeviceClass.HEAT,
+    ),
+    HeishaMonBinarySensorEntityDescription(
+        key="panasonic_heat_pump/main/External_Heater_State",
+        name="Aquarea External Heater State",
+        state=bit_to_bool,
+        device_class=BinarySensorDeviceClass.HEAT,
     ),
 )
 
@@ -163,5 +259,122 @@ SENSORS: tuple[HeishaMonSensorEntityDescription, ...] = (
         native_unit_of_measurement="W",
         state_class=SensorStateClass.MEASUREMENT,
         # original template states "force_update" FIXME
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Powerful_Mode_Time",
+        name="Aquarea Powerful Mode",
+        device_class=SensorDeviceClass.DURATION,
+        native_unit_of_measurement="Min",
+        state=read_power_mode_time,
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/ThreeWay_Valve_State",
+        name="Aquarea 3-way Valve",
+        state=read_threeway_valve,
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Outside_Pipe_Temp",
+        name="Aquarea Outdoor Pipe Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Z1_Heat_Request_Temp",
+        name="Aquarea Heatshift Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/DHW_Energy_Production",
+        name="Aquarea DHW Power Produced",
+        device_class=SensorDeviceClass.POWER,
+        native_unit_of_measurement="W",
+        state_class=SensorStateClass.MEASUREMENT,
+        # original template states "force_update" FIXME
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/DHW_Energy_Consumption",
+        name="Aquarea DHW Power Consumed",
+        device_class=SensorDeviceClass.POWER,
+        native_unit_of_measurement="W",
+        state_class=SensorStateClass.MEASUREMENT,
+        # original template states "force_update" FIXME
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Error",
+        name="Aquarea Last Error",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Main_Hex_Outlet_Temp",
+        name="Aquarea Main HEX Outlet Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Discharge_Temp",
+        name="Aquarea Discharge Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Inside_Pipe_Temp",
+        name="Aquarea Inside Pipe Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Defrost_Temp",
+        name="Aquarea Defrost Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Eva_Outlet_Temp",
+        name="Aquarea Eva Outlet Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Bypass_Outlet_Temp",
+        name="Aquarea Bypass Outlet Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Ipm_Temp",
+        name="Aquarea Ipm Temperature",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        native_unit_of_measurement="C°",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Fan1_Motor_Speed",
+        name="Aquarea Fan 1 Speed",
+        native_unit_of_measurement="R/min",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Fan2_Motor_Speed",
+        name="Aquarea Fan 2 Speed",
+        native_unit_of_measurement="R/min",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/High_Pressure",
+        name="Aquarea High pressure",
+        native_unit_of_measurement="Kgf/cm2",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Pump_Speed",
+        name="Aquarea Pump Speed",
+        native_unit_of_measurement="R/min",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Low_Pressure",
+        name="Aquarea Low Pressure",
+        native_unit_of_measurement="Kgf/cm2",
+    ),
+    HeishaMonSensorEntityDescription(
+        key="panasonic_heat_pump/main/Compressor_Current",
+        name="Aquarea Compressor Current",
+        device_class=SensorDeviceClass.CURRENT,
+        native_unit_of_measurement="A",
     ),
 )
---
+++ custom_components/heishamon/switch.py
@@ -0,0 +1,98 @@
+"""Support for HeishaMon controlled heatpumps through MQTT."""
+from __future__ import annotations
+import logging
+
+from homeassistant.components import mqtt
+from homeassistant.components.mqtt.client import async_publish
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.config_entries import SOURCE_IMPORT, ConfigEntry
+from homeassistant.core import HomeAssistant, callback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.issue_registry import IssueSeverity, async_create_issue
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+from homeassistant.util import slugify
+
+from .const import DOMAIN
+from .definitions import MQTT_SWITCHES, HeishaMonSwitchEntityDescription
+
+_LOGGER = logging.getLogger(__name__)
+
+# async_setup_platform should be defined if one wants to support config via configuration.yaml
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up HeishaMon sensors from config entry."""
+    async_add_entities(
+        HeishaMonMQTTSwitch(hass, description, config_entry)
+        for description in MQTT_SWITCHES
+    )
+
+
+class HeishaMonMQTTSwitch(SwitchEntity):
+    """Representation of a HeishaMon sensor that is updated via MQTT."""
+
+    entity_description: HeishaMonSwitchEntityDescription
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        description: HeishaMonSwitchEntityDescription,
+        config_entry: ConfigEntry,
+    ) -> None:
+        """Initialize the sensor."""
+        self.entity_description = description
+        self.hass = hass
+
+        slug = slugify(description.key.replace("/", "_"))
+        self.entity_id = f"sensor.{slug}"
+        self._attr_unique_id = f"{config_entry.entry_id}-{slug}"
+        self._optimistic = True  # for now we hardcode this
+
+    async def async_turn_on(self) -> None:
+        _LOGGER.info(f"Turning on heatpump {self.entity_description.name}")
+        await async_publish(
+            self.hass,
+            self.entity_description.command_topic,
+            self.entity_description.payload_on,
+            self.entity_description.qos,
+            self.entity_description.retain,
+            self.entity_description.encoding,
+        )
+        if self._optimistic:
+            self._state = True
+            self.async_write_ha_state()
+
+    async def async_turn_off(self) -> None:
+        _LOGGER.info(f"Turning off heatpump {self.entity_description.name}")
+        await async_publish(
+            self.hass,
+            self.entity_description.command_topic,
+            self.entity_description.payload_off,
+            self.entity_description.qos,
+            self.entity_description.retain,
+            self.entity_description.encoding,
+        )
+        if self._optimistic:
+            self._state = False
+            self.async_write_ha_state()
+
+    async def async_added_to_hass(self) -> None:
+        """Subscribe to MQTT events."""
+
+        @callback
+        def message_received(message):
+            """Handle new MQTT messages."""
+            if self.entity_description.state is not None:
+                self._attr_is_on = self.entity_description.state(message.payload)
+            else:
+                self._attr_is_on = message.payload
+
+            self.async_write_ha_state()
+
+        await mqtt.async_subscribe(
+            self.hass, self.entity_description.key, message_received, 1
+        )
