bc8db6d2a177ec1f333abfbfc3c6a816a718ec01
Grégoire Seux <grego_github@familleseux.net>
2023-10-07 11:32:23 +0200

    ✨ Allow heat/cool curves manipulation
    This introduces a small breaking change by removing some existing sensor
    entities and replacing them with a number entity
    
    Fix #114

--- custom_components/aquarea/definitions.py
+++ custom_components/aquarea/definitions.py
@@ -14,7 +14,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
 from homeassistant.components.switch import SwitchEntityDescription
 from homeassistant.components.select import SelectEntityDescription
-from homeassistant.components.number import NumberEntityDescription
+from homeassistant.components.number import (NumberEntityDescription, NumberDeviceClass)
 
 
 from homeassistant.components.sensor import (
@@ -305,7 +305,7 @@ def guess_shift_or_direct_and_clamp_min_max_values(
 
 
 def build_numbers(mqtt_prefix: str) -> list[HeishaMonNumberEntityDescription]:
-    return [
+    numbers = [
         HeishaMonNumberEntityDescription(
             heishamon_topic_id="SET5",  # also TOP27
             key=f"{mqtt_prefix}main/Z1_Heat_Request_Temp",
@@ -477,6 +477,66 @@ def build_numbers(mqtt_prefix: str) -> list[HeishaMonNumberEntityDescription]:
             entity_registry_enabled_default=False, # comes from the optional PCB: disabled by default
         ),
     ]
+    topic_ids = {
+            "1 Heat Target High": "TOP29",
+            "1 Heat Target Low": "TOP30",
+            "1 Heat Outside High": "TOP31",
+            "1 Heat Outside Low": "TOP32",
+            "1 Cool Target High": "TOP72",
+            "1 Cool Target Low": "TOP73",
+            "1 Cool Outside High": "TOP74",
+            "1 Cool Outside Low": "TOP75",
+            "2 Heat Target High": "TOP82",
+            "2 Heat Target Low": "TOP83",
+            "2 Heat Outside High": "TOP84",
+            "2 Heat Outside Low": "TOP85",
+            "2 Cool Target High": "TOP86",
+            "2 Cool Target Low": "TOP87",
+            "2 Cool Outside High": "TOP88",
+            "2 Cool Outside Low": "TOP89",
+    }
+    ranges = {
+            "Outside": [-15, 30],
+            "Target": [15, 60],
+    }
+    def dual_location(loc):
+        if loc == 'Target':
+            return 'Outside'
+        return 'Target'
+    for zone_id in [1, 2]:
+        for action in ['Cool', 'Heat']:
+            for point in ['High', 'Low']:
+                for loc in ['Target', 'Outside']:
+                    numbers.append(HeishaMonNumberEntityDescription(
+                        heishamon_topic_id=topic_ids[f"{zone_id} {action} {loc} {point}"],
+                        key=f"{mqtt_prefix}main/Z{zone_id}_{action}_Curve_{loc}_{point}_Temp",
+                        command_topic=f"{mqtt_prefix}commands/SetCurves",
+                        entity_category=EntityCategory.CONFIG,
+                        native_min_value=ranges[loc][0],
+                        native_max_value=ranges[loc][1],
+                        name=f"Aquarea Zone {zone_id} {loc} water temperature at {point.lower()}est {dual_location(loc).lower()} temperature on {action.lower()}ing curve",
+                        device_class=NumberDeviceClass.TEMPERATURE,
+                        native_unit_of_measurement="°C",
+                        # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
+                        entity_registry_enabled_default=(action=='Heat'),
+                        state_to_mqtt=write_curves_gen(zone_id, action, loc, point),
+                        )
+                    )
+    return numbers
+
+def write_curves_gen(zone_id: int, action: str, loc: str, point: str):
+    def write_curves(value: int) -> str:
+        json_doc = {
+                f"zone{zone_id}": {
+                    action.lower(): {
+                        loc.lower(): {
+                            point.lower(): int(value)
+                        }
+                    }
+                }
+            }
+        return json.dumps(json_doc)
+    return write_curves
 
 
 def build_selects(mqtt_prefix: str) -> list[HeishaMonSelectEntityDescription]:
@@ -865,38 +925,6 @@ def build_sensors(mqtt_prefix: str) -> list[HeishaMonSensorEntityDescription]:
             native_unit_of_measurement="°C",
         ),
         HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP29",
-            key=f"{mqtt_prefix}main/Z1_Heat_Curve_Target_High_Temp",
-            state_class=SensorStateClass.MEASUREMENT,
-            name="Aquarea Zone 1 Target water temperature at highest outside temperature on heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP30",
-            key=f"{mqtt_prefix}main/Z1_Heat_Curve_Target_Low_Temp",
-            state_class=SensorStateClass.MEASUREMENT,
-            name="Aquarea Zone 1 Target water temperature at lowest outside temperature on heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP31",
-            key=f"{mqtt_prefix}main/Z1_Heat_Curve_Outside_High_Temp",
-            state_class=SensorStateClass.MEASUREMENT,
-            name="Aquarea Zone 1 Highest outside temperature on the heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP32",
-            key=f"{mqtt_prefix}main/Z1_Heat_Curve_Outside_Low_Temp",
-            state_class=SensorStateClass.MEASUREMENT,
-            name="Aquarea Zone 1 Lowest outside temperature on the heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-        ),
-        HeishaMonSensorEntityDescription(
             heishamon_topic_id="TOP33",
             key=f"{mqtt_prefix}main/Room_Thermostat_Temp",
             state_class=SensorStateClass.MEASUREMENT,
@@ -1146,42 +1174,6 @@ def build_sensors(mqtt_prefix: str) -> list[HeishaMonSensorEntityDescription]:
             state_class=SensorStateClass.MEASUREMENT,
         ),
         HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP72",
-            key=f"{mqtt_prefix}main/Z1_Cool_Curve_Target_High_Temp",
-            name="Aquarea Zone 1 Target water temperature at highest outside temperature on cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP73",
-            key=f"{mqtt_prefix}main/Z1_Cool_Curve_Target_Low_Temp",
-            name="Aquarea Zone 1 Target water temperature at lowest outside temperature on cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP74",
-            key=f"{mqtt_prefix}main/Z1_Cool_Curve_Outside_High_Temp",
-            name="Aquarea Zone 1 Highest outside temperature on the cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP75",
-            key=f"{mqtt_prefix}main/Z1_Cool_Curve_Outside_Low_Temp",
-            name="Aquarea Zone 1 Lowest outside temperature on the cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
             heishamon_topic_id="TOP76",
             key=f"{mqtt_prefix}main/Heating_Mode",
             name="Aquarea Heating Mode",
@@ -1226,74 +1218,6 @@ def build_sensors(mqtt_prefix: str) -> list[HeishaMonSensorEntityDescription]:
             key=f"{mqtt_prefix}main/Cooling_Mode",
             name="Aquarea Cooling Mode",
             state=read_heating_mode,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP82",
-            key=f"{mqtt_prefix}main/Z2_Heat_Curve_Target_High_Temp",
-            name="Aquarea Zone 2 Target water temperature at highest outside temperature on heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP83",
-            key=f"{mqtt_prefix}main/Z2_Heat_Curve_Target_Low_Temp",
-            name="Aquarea Zone 2 Target water temperature at lowest outside temperature on heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP84",
-            key=f"{mqtt_prefix}main/Z2_Heat_Curve_Outside_High_Temp",
-            name="Aquarea Zone 2 Highest outside temperature on the heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP85",
-            key=f"{mqtt_prefix}main/Z2_Heat_Curve_Outside_Low_Temp",
-            name="Aquarea Zone 2 Lowest outside temperature on the heating curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP86",
-            key=f"{mqtt_prefix}main/Z2_Cool_Curve_Target_High_Temp",
-            name="Aquarea Zone 2 Target water temperature at highest outside temperature on cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP87",
-            key=f"{mqtt_prefix}main/Z2_Cool_Curve_Target_Low_Temp",
-            name="Aquarea Zone 2 Target water temperature at lowest outside temperature on cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP88",
-            key=f"{mqtt_prefix}main/Z2_Cool_Curve_Outside_High_Temp",
-            name="Aquarea Zone 2 Highest outside temperature on the cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
-            entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
-        ),
-        HeishaMonSensorEntityDescription(
-            heishamon_topic_id="TOP89",
-            key=f"{mqtt_prefix}main/Z2_Cool_Curve_Outside_Low_Temp",
-            name="Aquarea Zone 2 Lowest outside temperature on the cooling curve",
-            device_class=SensorDeviceClass.TEMPERATURE,
-            native_unit_of_measurement="°C",
-            state_class=SensorStateClass.MEASUREMENT,
             entity_registry_enabled_default=False,  # by default we hide all options related to less common setup (cooling, buffer, solar and pool)
         ),
         HeishaMonSensorEntityDescription(
