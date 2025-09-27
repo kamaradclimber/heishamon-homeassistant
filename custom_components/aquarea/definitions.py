dff71945fb87a890a96286715c111b69ddec4cca
Grégoire Seux <grego_github@familleseux.net>
2025-09-27 08:57:19 +0200

    Increase max temp to 75°C
    Fix #299

--- custom_components/aquarea/definitions.py
+++ custom_components/aquarea/definitions.py
@@ -744,7 +744,7 @@ def build_numbers(mqtt_prefix: str) -> list[HeishaMonNumberEntityDescription]:
     ranges = {
         "Heat": {
             "Outside": [-20, 30],
-            "Target": [15, 70],
+            "Target": [15, 75],
         },
         "Cool": {
             "Outside": [15, 30],
